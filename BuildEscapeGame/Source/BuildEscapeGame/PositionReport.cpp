84eb35d652c245302ad1ed28a0e1a8051c2abb68
rain330047@outlook.com <rain330047@outlook.com>
2018-09-29 00:11:21 +0800

    Get access to component owner name

--- BuildEscapeGame/Source/BuildEscapeGame/PositionReport.cpp
+++ BuildEscapeGame/Source/BuildEscapeGame/PositionReport.cpp
@@ -1,5 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
+#include "GameFramework/Actor.h"
 #include "PositionReport.h"
 
 
@@ -20,6 +20,8 @@ void UPositionReport::BeginPlay()
 	Super::BeginPlay();
 
 	// ...
+	FString ObjectName = GetOwner()->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("Position report reporting for %s"), *ObjectName);
 	
 }
 
